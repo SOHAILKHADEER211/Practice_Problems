e5c065d7db2147a1e99fd07f0dbec40d49d2f611
Sohailkhadeer <starsall64@gmail.com>
2022-09-09 10:27:47 +0530

    Data types commit

---
+++ Practice_Problems/src/main/java/New/Data_Types.java
@@ -0,0 +1,26 @@
+package New;
+
+public class Data_Types {
+    static int a = 5;
+    static char abcd;
+    static long c = 100000000;
+    static float d = 5.5F;
+    static double e = 55.129323214d;
+    static short f = 8;
+    static boolean g = true;
+    static byte h = 127;
+
+    public static void main(String[] args) {
+        System.out.println("int = " + a);
+        System.out.println("ch = " + abcd);
+        System.out.println("long = " + c);
+        System.out.println("float = " + d);
+        System.out.println("double = " + e);
+        System.out.println("short = " + f);
+        System.out.println("boolean = " + g);
+        System.out.println("byte = " + h);
+
+    }
+
+}
+
