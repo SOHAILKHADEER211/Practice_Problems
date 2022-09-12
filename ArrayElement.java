aad28ad40ec62a7de236605b89ef6ceb8bf707bc
Sohailkhadeer <starsall64@gmail.com>
2022-09-13 00:35:37 +0530

    Added day 3 array progrm

---
+++ ArrayElement.java
@@ -0,0 +1,20 @@
+package New3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayElement {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 5, 7, 3, 345, 8,50,78,6};
+        System.out.println("given below are array elements");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            System.out.println("");
+            /*System.out.println(Arrays.toString(array));*/
+        }
+    }
+}
+
+
+
+
